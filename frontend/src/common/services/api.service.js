aa7d07855f41d1243e52558d92dd97ace3698396
Jason Ngan <jason.ngan@shopee.com>
2021-03-08 15:43:37 +0800

    add config

--- frontend/src/common/services/api.service.js
+++ frontend/src/common/services/api.service.js
@@ -30,15 +30,15 @@ const ApiService = {
   },
 
   post(resource, data) {
-    return axios.post(resource, data)
+    return axios.post(`${base_url + resource}`, data);
   },
 
   put(resource, data) {
-    return axios.put(resource, data)
+    return axios.put(`${base_url + resource}`, data);
   },
 
   delete(resource) {
-    return axios.delete(resource)
+    return axios.delete(`${base_url + resource}`);
   }
 
 }
